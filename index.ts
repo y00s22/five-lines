bb069c4e5f12de679d4549c44fc399cd016c2d5c
y00s22 <ryusuy22@gmail.com>
2023-08-15 21:15:24 +0900

    규칙: switch를 사용하지 말 것 - 열거형 Tile을 클래스로 대체한다 - 기존 map을 새로운 클래스로 변환하기 위한 함수 구현

--- index.ts
+++ index.ts
@@ -246,7 +246,7 @@ class Down implements Input2 {
 
 let playerx = 1;
 let playery = 1;
-let map: Tile[][] = [
+let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -254,6 +254,41 @@ let map: Tile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
+
+let map: Tile2[][];
+function asssertExhausted(x: never): never {
+  throw new Error("Unexpected objcet: " + x);
+}
+
+function transformTile(tile: RawTile) {
+  switch (tile) {
+    case RawTile.AIR: return new Air();
+    case RawTile.FLUX: return new Flux();
+    case RawTile.UNBREAKABLE: return new Unbreakable();
+    case RawTile.PLAYER: return new Player();
+    case RawTile.STONE: return new Stone();
+    case RawTile.FALLING_STONE: return new FallingStone();
+    case RawTile.BOX: return new Box(); 
+    case RawTile.FALLING_BOX: return new FallingBox();
+    case RawTile.KEY1: return new Key1(); 
+    case RawTile.LOCK1: return new Lock1();
+    case RawTile.KEY2: return new Key2(); 
+    case RawTile.LOCK2: return new Lock2();
+    default: asssertExhausted(tile);
+  }
+}
+
+function transformMap() {
+  map = new Array(rawMap.length);
+
+  for (let y = 0; y < rawMap.length; y++) {
+    map[y] = new Array(rawMap[y].length);
+
+    for (let x = 0; x < rawMap[y].length; x++) {
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
 
 let inputs: Input2[] = [];
 
@@ -416,6 +451,7 @@ function gameLoop() {
 }
 
 window.onload = () => {
+  transformMap();
   gameLoop();
 }
 
