64a04ac6f77c5bd38ff215f37d26c573c8fd0de1
y00s22 <ryusuy22@gmail.com>
2023-08-23 01:27:18 +0900

    조건의 의미를 파악하여 if문의 본문을 동일하게 한 뒤 if문을 결합한다

--- index.ts
+++ index.ts
@@ -545,13 +545,10 @@ function updateMap() {
 }
 
 function updateTile(y: number, x: number) {
-  if ((map[y][x].isStony())
-    && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
-    map[y][x] = new Air();
-  } else if ((map[y][x].isBoxy())
-    && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
+  if ((map[y][x].isStony() && map[y + 1][x].isAir())
+    || (map[y][x].isBoxy() && map[y + 1][x].isAir())) {
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
